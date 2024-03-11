8e376d0e3ed3b82a0b43a31405e9752212c7009d
Wendddddiiii <135685188+Wendddddiiii@users.noreply.github.com>
2024-03-11 18:05:12 +1100

    Delete build directory

--- build/libc-8e59cfa71373f3e0/build_script_build-8e59cfa71373f3e0.d
+++
@@ -1,5 +0,0 @@
-/Users/wenditang/Desktop/rust/guessing_game/target/debug/build/libc-8e59cfa71373f3e0/build_script_build-8e59cfa71373f3e0: /Users/wenditang/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.153/build.rs
-
-/Users/wenditang/Desktop/rust/guessing_game/target/debug/build/libc-8e59cfa71373f3e0/build_script_build-8e59cfa71373f3e0.d: /Users/wenditang/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.153/build.rs
-
-/Users/wenditang/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.153/build.rs:
