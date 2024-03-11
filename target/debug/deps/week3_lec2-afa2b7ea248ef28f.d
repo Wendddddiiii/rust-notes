36f6edf8d0cd2be799099c761f489bb0a1438db4
Wendddddiiii <135685188+Wendddddiiii@users.noreply.github.com>
2024-03-11 17:42:17 +1100

    Delete target directory

--- target/debug/deps/week3_lec2-afa2b7ea248ef28f.d
+++
@@ -1,5 +0,0 @@
-/Users/wenditang/Desktop/rust/week3_lec2/target/debug/deps/week3_lec2-afa2b7ea248ef28f: src/main.rs
-
-/Users/wenditang/Desktop/rust/week3_lec2/target/debug/deps/week3_lec2-afa2b7ea248ef28f.d: src/main.rs
-
-src/main.rs:
--- target/debug/week3_lec2.d
+++
@@ -1,1 +0,0 @@
-/Users/wenditang/Desktop/rust/week3_lec2/target/debug/week3_lec2: /Users/wenditang/Desktop/rust/week3_lec2/src/main.rs
